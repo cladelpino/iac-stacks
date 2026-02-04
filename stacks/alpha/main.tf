74d0005bab87f7a89937ed47b8402a6c4a1f2f22
cladelpino <cladelpino@gmail.com>
2026-02-04 20:48:32 -0300

    Fix alpha id

--- stacks/alpha/main.tf
+++ stacks/alpha/main.tf
@@ -8,7 +8,7 @@ data "azurerm_resource_group" "rg" {
 
 
 locals {
-  storage_account_name = "alpha-unique-asdasd"
+  storage_account_name = "alphauniqueasdasd"
 }
 
 resource "azurerm_storage_account" "sa" {
